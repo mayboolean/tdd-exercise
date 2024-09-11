2782c1d2eae2b7cafa3dd92183231deaf17af9b6
Maybellene <maybelleneaung@gmail.com>
2024-09-11 18:05:16 -0400

    worked on function and added assert tests

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
 def blackjack_score(hand):
-    pass
+    
+    score = 0
+    ace_count = 0
+
+    if not (2 <= len(hand) <= 5):
+        return None
+    
+    for card in hand:
+        if card in VALID_CARDS:
+            if isinstance(card, int):
+                score += card
+            elif card == "Jack" or card == "Queen" or card == "King":
+                score += 10
+            elif card == "Ace":
+                ace_count += 1
+            else:
+                return None
+        else:
+            return None
+            
+    ace_value = 11
+    for i in range(ace_count):
+        if score + ace_value > 21:
+            ace_value = 1
+        score += ace_value    
+    
+    if score > 21:
+        return("Bust!")
+    
+    return score
--- test_blackjack_score.py
+++ test_blackjack_score.py
@@ -5,30 +5,48 @@ import pytest
 def test_score_for_pair_of_number_cards():
   # Arrange
   hand = [3, 4]
-
+  expected_score = 7
   # Act
   score = blackjack_score(hand)
 
   # Assert <-- Write assert statement here
-  
+  assert score == expected_score
 
-@pytest.mark.skip(reason="no way of currently testing this")
+#@pytest.mark.skip(reason="no way of currently testing this")
 def test_facecards_have_values_calculated_correctly():
-  pass
+  hand_1 = ["King", "Queen"]
+  hand_2 = ["Jack", 9]
+  assert blackjack_score(hand_1) == 20
+  assert blackjack_score(hand_2) == 19
 
-@pytest.mark.skip(reason="no way of currently testing this")
+#@pytest.mark.skip(reason="no way of currently testing this")
 def test_calculates_aces_as_11_where_it_does_not_go_over_21():
-  pass
+  hand_1 = ["Ace","King"]
+  hand_2 = [4,"Ace"]
+  hand_3 = [5,4,"Ace"]
+  assert blackjack_score(hand_1) == 21
+  assert blackjack_score(hand_2) == 15
+  assert blackjack_score(hand_3) == 20
 
-
-@pytest.mark.skip(reason="no way of currently testing this")
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_calculates_aces_as_1_where_11_would_bust():
-  pass
+  hand_1 = ["Queen","King","Ace"]
+  hand_2 = ["Ace",10,9]
+  
+  assert blackjack_score(hand_1) == 21
+  assert blackjack_score(hand_2) == 20
 
-@pytest.mark.skip(reason="no way of currently testing this")
+# @pytest.mark.skip(reason="no way of currently testing this")
 def test_returns_invalid_for_invalid_cards():
-  pass
+  hand_1 = []
+  hand_2 = ["Queen"]
+  hand_3 = [1,"Joker"]
+  hand_4 = [1,2,3,4,5,6]
 
+  assert blackjack_score(hand_1) == None
+  assert blackjack_score(hand_2) == None
+  assert blackjack_score(hand_3) == None
+  assert blackjack_score(hand_4) == None
 
 @pytest.mark.skip(reason="no way of currently testing this")
 def test_returns_invalid_for_list_length_greater_than_5():
